25e1dabb3e6ba0bd983852249d3a186e9ec594cd
Nazar Banira <nazarbanira@gmail.com>
2021-08-01 20:59:48 +0300

    I dont know

--- Junior-Programmer-Starter-Files/Assets/Scripts/UI/UIMainScene.cs
+++ Junior-Programmer-Starter-Files/Assets/Scripts/UI/UIMainScene.cs
@@ -73,4 +73,7 @@ public class UIMainScene : MonoBehaviour
             InfoPopup.Name.text = content.GetName();
         }
     }
+    public void BackToMenu(){
+        SceneManager.LoadScene(0);
+    }
 }
